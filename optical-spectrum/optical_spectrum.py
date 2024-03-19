76a492e7dea1e076d6315afacf823905e62a725d
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2024-03-19 13:07:05 +0100

    use lower case for parameters

--- optical-spectrum/optical_spectrum.py
+++ optical-spectrum/optical_spectrum.py
@@ -20,14 +20,14 @@ log.addHandler(logging.NullHandler())
 class ReadoutPowerLevelProcedure(Procedure):
 
     resolution_bandwidth = ListParameter(
-        "Bandwidth Resolution",
+        "bandwidth resolution",
         default=0.1e-9,
         units="m",
         choices=[0.02e-9, 0.05e-9, 0.1e-9, 0.2e-9, 0.5e-9, 1e-9, 2e-9],
     )
 
     wavelength_start = FloatParameter(
-        "Wavelength Start",
+        "wavelength start",
         default=7.65e-7,
         units="m",
         minimum=50e-9,
@@ -35,7 +35,7 @@ class ReadoutPowerLevelProcedure(Procedure):
     )
 
     wavelength_stop = FloatParameter(
-        "Wavelength Stop",
+        "wavelength stop",
         default=8.05e-7,
         units="m",
         minimum=600e-9,
@@ -43,7 +43,7 @@ class ReadoutPowerLevelProcedure(Procedure):
     )
 
     reference_level = FloatParameter(
-        "Reference Level",
+        "reference level",
         default=0.0,
         units="dBm",
         minimum=-90.0,
@@ -51,14 +51,14 @@ class ReadoutPowerLevelProcedure(Procedure):
     )
 
     level_position = IntegerParameter(
-        "Level Position",
+        "level position",
         default=8,
         minimum=1,
         maximum=12,
     )
 
     sample_number = IntegerParameter(
-        "Number of sampling points",
+        "number of sampling points",
         default=1001,
         minimum=101,
         maximum=50001,
@@ -70,7 +70,7 @@ class ReadoutPowerLevelProcedure(Procedure):
         default=False,
     )
 
-    DATA_COLUMNS = ["Wavelength", "Power Level"]
+    DATA_COLUMNS = ["wavelength", "power level"]
 
     def startup(self):
         log.info("Connecting to AQ6370D")
@@ -95,7 +95,7 @@ class ReadoutPowerLevelProcedure(Procedure):
         for x, y in zip(x_data, y_data):
             self.emit(
                 "results",
-                {"Wavelength": x, "Power Level": y},
+                {"wavelength": x, "power level": y},
             )
 
         del self.osa
@@ -123,8 +123,8 @@ class MainWindow(ManagedWindow):
                 "manual_sample_number",
                 "sample_number",
             ],
-            x_axis="Wavelength",
-            y_axis="Power Level",
+            x_axis="wavelength",
+            y_axis="power level",
             enable_file_input=True,
         )
         self.setWindowTitle("Optical Spectral Analyzer AQ6370D")
